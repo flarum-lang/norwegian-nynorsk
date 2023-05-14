7f83c603ebd2c91d161d386b7002422ede4b28db
Robert Korulczyk <robert@korulczyk.pl>
2023-05-14 16:12:00 +0200

    Update repository template.

--- locale/config.js
+++ locale/config.js
@@ -1,6 +1,6 @@
-// source: https://unpkg.com/dayjs@v1.10.4/locale/xx.js
-// see: https://github.com/iamkun/dayjs/blob/v1.10.4/src/locale/xx.js
+// source: https://unpkg.com/dayjs@v1.10.4/locale/nn.js
+// see: https://github.com/iamkun/dayjs/blob/v1.10.4/src/locale/nn.js
 
-// TODO: copy compiled translations from https://unpkg.com/dayjs@v1.10.4/locale/xx.js
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t(require("dayjs")):"function"==typeof define&&define.amd?define(["dayjs"],t):e.dayjs_locale_nn=t(e.dayjs)}(this,function(e){"use strict";e=e&&e.hasOwnProperty("default")?e.default:e;var t={name:"nn",weekdays:"sundag_måndag_tysdag_onsdag_torsdag_fredag_laurdag".split("_"),weekdaysShort:"sun_mån_tys_ons_tor_fre_lau".split("_"),weekdaysMin:"su_må_ty_on_to_fr_la".split("_"),months:"januar_februar_mars_april_mai_juni_juli_august_september_oktober_november_desember".split("_"),monthsShort:"jan_feb_mar_apr_mai_jun_jul_aug_sep_okt_nov_des".split("_"),ordinal:function(e){return e+"."},weekStart:1,relativeTime:{future:"om %s",past:"for %s sidan",s:"nokre sekund",m:"eitt minutt",mm:"%d minutt",h:"ein time",hh:"%d timar",d:"ein dag",dd:"%d dagar",M:"ein månad",MM:"%d månadar",y:"eitt år",yy:"%d år"},formats:{LT:"HH:mm",LTS:"HH:mm:ss",L:"DD.MM.YYYY",LL:"D. MMMM YYYY",LLL:"D. MMMM YYYY [kl.] H:mm",LLLL:"dddd D. MMMM YYYY [kl.] HH:mm"}};return e.locale(t,null,!0),t});
 
-dayjs.locale('xx');
+dayjs.locale('nn');
